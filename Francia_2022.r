627f53d597611700f5428f6cd90326d47b8af554
Raffaele Mastrolonardo <raffaele.mastrolonardo@gmail.com>
2022-04-26 16:11:50 +0200

    Update Francia_2022.r

--- Francia_2022.r
+++ Francia_2022.r
@@ -11,7 +11,8 @@ redditi_2019_comuni_francia <- read.csv2("https://raw.githubusercontent.com/leop
 
 #ANALISI
 #Unisco i due dataset
-francia_ballottaggio_2022 <- left_join(francia_secondo_turno_2022,redditi_2019_comuni_francia, by="CODGEO")
+francia_ballottaggio_2022 <- left_join(francia_secondo_turno_2022,redditi_2019_comuni_francia, by="CODGEO") %>%
+  mutate_if(is.numeric, round, 2)
 
 francia_ballottaggio_2022$TP6019 <- sapply(francia_ballottaggio_2022$TP6019, as.character) 
 
@@ -21,20 +22,22 @@ francia_ballottaggio_2022$TP6019[is.na(francia_ballottaggio_2022$TP6019)] <- ""
 #Creo due nuove colonne con il risultato
 
 francia_ballottaggio_2022 <- francia_ballottaggio_2022 %>%
-  mutate(Vantaggio_Macron=ifelse(MACRON.exp == "50.00000", "pari", 
-                                 ifelse(MACRON.exp > "50.01" & MACRON.exp <"54.99","Macron 1",
-                                        ifelse(MACRON.exp > "55.00" & MACRON.exp < "59.99","Macron 2",
-                                               ifelse(MACRON.exp > "60.00" & MACRON.exp < "64.99","Macron 3",
-                                                      ifelse(MACRON.exp > "65.00" & MACRON.exp < "69.99","Macron 4",
-                                                             ifelse(MACRON.exp > "70.00" & MACRON.exp < "74.99","Macron 5",
-                                                                    ifelse(MACRON.exp > "75.00","Macron 6",""))))))))%>%
-  mutate(Vantaggio_LePen=ifelse(MACRON.exp == "50.00000", "pari",
-           ifelse(LE.PEN.exp > "50.01" & LE.PEN.exp <"54.99","Le Pen 1",
-                                        ifelse(LE.PEN.exp > "55.00" & LE.PEN.exp < "59.99","Le Pen 2",
-                                               ifelse(LE.PEN.exp > "60.00" & LE.PEN.exp < "64.99","Le Pen 3",
-                                                      ifelse(LE.PEN.exp > "65.00" & LE.PEN.exp < "69.99","Le Pen 4",
-                                                             ifelse(LE.PEN.exp > "70.00" & LE.PEN.exp < "74.99","Le Pen 5",
-                                                                    ifelse(LE.PEN.exp > "75.00","Le Pen 6",""))))))))%>%
+  mutate(Vantaggio_Macron=ifelse(MACRON.exp < "50.00","",
+                                        ifelse(MACRON.exp == "50.00", "pari",
+                                        ifelse(MACRON.exp > "50.00" & MACRON.exp <="54.99","Macron 1",
+                                        ifelse(MACRON.exp > "54.99" & MACRON.exp <= "59.99","Macron 2",
+                                               ifelse(MACRON.exp > "59.99" & MACRON.exp <= "64.99","Macron 3",
+                                                      ifelse(MACRON.exp > "64.99" & MACRON.exp <= "69.99","Macron 4",
+                                                             ifelse(MACRON.exp > "69.99" & MACRON.exp <= "74.99","Macron 5",
+                                                                    ifelse(MACRON.exp > "74.99","Macron 6","")))))))))%>%
+  mutate(Vantaggio_LePen=ifelse(LE.PEN.exp < "50.00","",
+                                ifelse(LE.PEN.exp == "50.00", "pari",
+                                       ifelse(LE.PEN.exp > "50.00" & LE.PEN.exp <="54.99","Le Pen 1",
+                                        ifelse(LE.PEN.exp > "54.99" & LE.PEN.exp <= "59.99","Le Pen 2",
+                                               ifelse(LE.PEN.exp > "59.99" & LE.PEN.exp <= "64.99","Le Pen 3",
+                                                      ifelse(LE.PEN.exp > "64.99" & LE.PEN.exp <= "69.99","Le Pen 4",
+                                                             ifelse(LE.PEN.exp > "69.99" & LE.PEN.exp <= "74.99","Le Pen 5",
+                                                                    ifelse(LE.PEN.exp > "74.99","Le Pen 6","")))))))))%>%
   unite(Risultato, c("Vantaggio_Macron", "Vantaggio_LePen"), sep = "")
 
 
